13d7983a8297c95a996aa1429e18c3a277a8a2c7
SpencerTower <spencerjtower1@gmail.com>
2022-01-26 08:49:27 -0800

    adds hub module, updates driver and vendor, updates readme

---
+++ clients/logger.js
@@ -0,0 +1,58 @@
+'use strict';
+
+
+// Listens to all events
+// Logs event, timestamp, and payload for every event
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Listens to ALL events in the Event Pool.
+// Logs a timestamp and the payload of every event.
+
+// const eventPool = require('./globalEvent.js');
+
+
+// eventPool.on('pickup', (payload) => {
+//   const event = {
+//     'event': 'pickup',
+//     'time': new Date(),
+//     payload,
+//   };
+//   console.log(event);
+// });
+
+// eventPool.on('transit', (payload) => {
+//   const event = {
+//     'event': 'in-transit',
+//     'time': new Date(),
+//     payload,
+//   };
+//   console.log(event);
+// });
+
+// eventPool.on('delivered', (payload) => {
+//   const event = {
+//     'event': 'delivered',
+//     'time': new Date(),
+//     payload,
+//   };
+//   console.log(event);
+// });
+
+// // .on for each function, calls function that logs each event
+
+
+// // }
--- modules/globalEvent.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-// import Events constructor
-const EventEmitter = require('events'); // similar to express - have to create a singleton
-
-const eventPool = new EventEmitter(); // singleton for our event pool
-
-module.exports = eventPool;
